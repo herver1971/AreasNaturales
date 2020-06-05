d6c041081f1f38ea3d05cd575e5f1806554b575c
Hernan <hernan.lopez@gmail.com>
2020-06-05 14:44:08 -0300

    Actualizacion diaria

--- script.sh
+++ script.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 wget -O UICN1b.osm --post-file=Consultas/UICN1bquery.osm "https://overpass-api.de/api/interpreter"
+sleep 5m
 wget -O UICN1.osm --post-file=Consultas/UICN1query.osm "https://overpass-api.de/api/interpreter"
+sleep 5m
 wget -O UICN2.osm --post-file=Consultas/UICN2query.osm "https://overpass-api.de/api/interpreter"
+sleep 5m
 wget -O UICN3.osm --post-file=Consultas/UICN3query.osm "https://overpass-api.de/api/interpreter"
+sleep 5m
 wget -O UICN4.osm --post-file=Consultas/UICN4query.osm "https://overpass-api.de/api/interpreter"
+sleep 5m
 wget -O UICN5.osm --post-file=Consultas/UICN5query.osm "https://overpass-api.de/api/interpreter"
+sleep 5m
 wget -O UICN6.osm --post-file=Consultas/UICN6query.osm "https://overpass-api.de/api/interpreter"
+sleep 5m
 wget -O UICN7.osm --post-file=Consultas/UICN7query.osm "https://overpass-api.de/api/interpreter"
 git status
 git add *
 git commit -m "Actualizacion diaria"
-git push origin master
+git push master origin
