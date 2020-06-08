8309ffd1808c1c215aec0bc70fdf4cb2cc1c46e8
Hernan <hernan.lopez@gmail.com>
2020-06-08 12:18:35 -0300

    Actualizacion diaria

--- script.sh
+++ script.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-wget -O UICN1b.osm --post-file=Consultas/UICN1bquery.osm "https://overpass-api.de/api/interpreter"
+/usr/bin/wget -O UICN1b.osm --post-file=Consultas/UICN1bquery.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-wget -O UICN1.osm --post-file=Consultas/UICN1query.osm "https://overpass-api.de/api/interpreter"
+/usr/bin/wget -O UICN1.osm --post-file=Consultas/UICN1query.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-wget -O UICN2.osm --post-file=Consultas/UICN2query.osm "https://overpass-api.de/api/interpreter"
+/usr/bin/wget -O UICN2.osm --post-file=Consultas/UICN2query.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-wget -O UICN3.osm --post-file=Consultas/UICN3query.osm "https://overpass-api.de/api/interpreter"
+/usr/bin/wget -O UICN3.osm --post-file=Consultas/UICN3query.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-wget -O UICN4.osm --post-file=Consultas/UICN4query.osm "https://overpass-api.de/api/interpreter"
+/usr/bin/wget -O UICN4.osm --post-file=Consultas/UICN4query.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-wget -O UICN5.osm --post-file=Consultas/UICN5query.osm "https://overpass-api.de/api/interpreter"
+/usr/bin/wget -O UICN5.osm --post-file=Consultas/UICN5query.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-wget -O UICN6.osm --post-file=Consultas/UICN6query.osm "https://overpass-api.de/api/interpreter"
+/usr/bin/wget -O UICN6.osm --post-file=Consultas/UICN6query.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-wget -O UICN7.osm --post-file=Consultas/UICN7query.osm "https://overpass-api.de/api/interpreter"
+/usr/bin/wget -O UICN7.osm --post-file=Consultas/UICN7query.osm "https://overpass-api.de/api/interpreter"
+sleep 5m
+/usr/bin/wget -O ramsar.osm --post-file=Consultas/ramsar.osm "https://overpass-api.de/api/interpreter"
 git status
 git add *
 git commit -m "Actualizacion diaria"
