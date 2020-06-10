6faace5d5ec642664de1096e127987deec749554
Hernan <hernan.lopez@gmail.com>
2020-06-10 02:06:26 -0300

    Actualizacion diaria

--- script.sh
+++ script.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-/usr/bin/wget -O UICN1b.osm --post-file=Consultas/UICN1bquery.osm "https://overpass-api.de/api/interpreter"
+wget -O /media/Dona/ProyectosGIT/AreasNaturales/UICN1b.osm --post-file=/media/Dona/ProyectosGIT/AreasNaturales/Consultas/UICN1bquery.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-/usr/bin/wget -O UICN1.osm --post-file=Consultas/UICN1query.osm "https://overpass-api.de/api/interpreter"
+wget -O /media/Dona/ProyectosGIT/AreasNaturales/UICN1.osm --post-file=/media/Dona/ProyectosGIT/AreasNaturales/Consultas/UICN1query.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-/usr/bin/wget -O UICN2.osm --post-file=Consultas/UICN2query.osm "https://overpass-api.de/api/interpreter"
+wget -O /media/Dona/ProyectosGIT/AreasNaturales/UICN2.osm --post-file=/media/Dona/ProyectosGIT/AreasNaturales/Consultas/UICN2query.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-/usr/bin/wget -O UICN3.osm --post-file=Consultas/UICN3query.osm "https://overpass-api.de/api/interpreter"
+wget -O /media/Dona/ProyectosGIT/AreasNaturales/UICN3.osm --post-file=/media/Dona/ProyectosGIT/AreasNaturales/Consultas/UICN3query.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-/usr/bin/wget -O UICN4.osm --post-file=Consultas/UICN4query.osm "https://overpass-api.de/api/interpreter"
+wget -O /media/Dona/ProyectosGIT/AreasNaturales/UICN4.osm --post-file=/media/Dona/ProyectosGIT/AreasNaturales/Consultas/UICN4query.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-/usr/bin/wget -O UICN5.osm --post-file=Consultas/UICN5query.osm "https://overpass-api.de/api/interpreter"
+wget -O /media/Dona/ProyectosGIT/AreasNaturales/UICN5.osm --post-file=/media/Dona/ProyectosGIT/AreasNaturales/Consultas/UICN5query.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-/usr/bin/wget -O UICN6.osm --post-file=Consultas/UICN6query.osm "https://overpass-api.de/api/interpreter"
+wget -O /media/Dona/ProyectosGIT/AreasNaturales/UICN6.osm --post-file=/media/Dona/ProyectosGIT/AreasNaturales/Consultas/UICN6query.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-/usr/bin/wget -O UICN7.osm --post-file=Consultas/UICN7query.osm "https://overpass-api.de/api/interpreter"
+wget -O /media/Dona/ProyectosGIT/AreasNaturales/UICN7.osm --post-file=/media/Dona/ProyectosGIT/AreasNaturales/Consultas/UICN7query.osm "https://overpass-api.de/api/interpreter"
 sleep 5m
-/usr/bin/wget -O ramsar.osm --post-file=Consultas/ramsar.osm "https://overpass-api.de/api/interpreter"
+wget -O /media/Dona/ProyectosGIT/AreasNaturales/ramsar.osm --post-file=/media/Dona/ProyectosGIT/AreasNaturales/Consultas/ramsar.osm "https://overpass-api.de/api/interpreter"
 git status
 git add *
 git commit -m "Actualizacion diaria"
